367df5136140da34843335ca32534d465d2dc1de
Aron <aronb21@ru.is>
2021-09-16 20:11:59 +0000

    direction_selection

--- Assignment_8.py
+++ Assignment_8.py
@@ -1,44 +1,47 @@
-# -1. Evaluate player location
-# 0. Possible directions are evaluated
-# 1. Player gets info about valid directions
+# 1. Possible directions are evaluated
 # 2. Player selects a direction
 # 3. direction is evaluated, if false go to 2.
 # 4. Player moves to the new location
 # 5. If player is at location 3,1, he wins, else he goes to 1.
 
-# .-1
+# .1
 def evaluate_direction(location):
-    victory_condition = False
-    if location == [1, 1] or location == [2,1] or location[3,1]:
-        possible_directions = "(N)orth"
+    if location == [1, 1] or location == [2,1]:
+        possible_directions = "(N)orth."
         
-
     elif location == [1, 2]:
-        possible_directions = "(N)orth", "(E)ast", "(S)outh"
+        possible_directions = "(N)orth or (E)ast or (S)outh."
         
-
     elif location == [1, 3]:
-        possible_directions = "(E)ast", "(S)outh"
+        possible_directions = "(E)ast or (S)outh."
         
-
     elif location == [2, 2] or location == [3, 3]:
-        possible_directions = "(S)outh", "(W)est"
+        possible_directions = "(S)outh or (W)est."
         
-
     elif location == [2, 3]:
-        possible_directions = "(E)ast", "(W)est"
+        possible_directions = "(E)ast or (W)est."
         
-
     elif location == [3, 2]:
-        possible_directions = "(N)orth", "(S)outh"
+        possible_directions = "(N)orth or (S)outh."
+
+    return possible_directions
+
+# 2.
+def direction_selection(location):
+
+    location_string = location.split(" or ")
+    while True: #finna eitthvað annað en True
+        direction_selection = input('Direction:')
+        for direction in location_string:
+            if direction[1] == direction_selection.upper:
+                return direction_selection
+        print("Not a valid direction!", direction[1], direction_selection)
         
+                        
+
+# 3.
+ 
     
-    elif location == [3, 1]:
-        victory_condition = True
-
-    return [possible_directions, victory_condition]
-
-
 # 4.
 def move(direction,location):
     if direction.lower == "n":
@@ -70,4 +73,7 @@ def move(direction,location):
 
     return location
 
-
+location = [1,1]
+location_string = evaluate_direction(location)
+print(location_string)
+direction_selection(location_string)
